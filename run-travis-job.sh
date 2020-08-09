b936cfebb6b3adca702f2a0d6ace8114c94dee42
Michele Lacchia <michelelacchia@gmail.com>
2020-08-09 05:08:38 +0200

    fix: run Tarpaulin with 1 test thread

--- run-travis-job.sh
+++ run-travis-job.sh
@@ -8,5 +8,5 @@ if [ "$COV" = "yes" ]
 then
     cargo install cargo-tarpaulin
     cargo tarpaulin -v --ignore-tests \
-        --ciserver travis-ci --coveralls "$TRAVIS_JOB_ID"
+        --ciserver travis-ci --coveralls "$TRAVIS_JOB_ID" -- --test-threads 1
 fi
